05be4e2a70d2a622b59f339dda7269ee18bb5aa4
Eliarlan Macedo <eliarlanrodrigues7@gmail.com>
2021-09-02 19:13:28 -0300

    23.1. Criando o módulo Dashboard

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,17 @@
+import { AuthGuard } from './../seguranca/auth.guard';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+
+const routes: Routes = [
+  { path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_PESQUISAR_LANCAMENTO'] }
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
