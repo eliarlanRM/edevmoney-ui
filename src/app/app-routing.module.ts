df39414c56bbb4998f86d29062b4aca612d59c4e
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2022-07-01 21:27:07 -0300

    Criando a navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LancamentosPesquisaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
