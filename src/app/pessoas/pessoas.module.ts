e5272761a7697f09be476c6e711c77b3848b3016
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2022-07-06 22:25:43 -0300

    Prototipo tela de cadastro de pessoas

--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
@@ -10,13 +11,13 @@ import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.co
 import { PessoasRoutingModule } from './pessoas-routing.module';
 
 
-
 @NgModule({
   declarations: [PessoasPesquisaComponent, PessoasCadastroComponent],
   imports: [
     CommonModule,
     TableModule,
     TooltipModule,
+    InputMaskModule,
     ButtonModule,
     InputTextModule,
     PessoasRoutingModule
